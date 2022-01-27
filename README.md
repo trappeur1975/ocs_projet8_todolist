c51c9f02b446339139f1218f29fcbf71129bfb76
Tr@ppeur1975 <trappeur1975photmail.com>
2022-01-27 20:08:16 +0400

    migration from symfony 3.4 to symfony 5.4.2 and correction of depreciation

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ https://openclassrooms.com/projects/ameliorer-un-projet-existant-1
 
 --------------------------
 16/01/2022 : creation of the github repository and update of the original symfony project in 3.1 to symfony 3.4 (correction of deprecations) 
+
+27/01/2022 : migration from symfony 3.4 to symfony 5.4.2 and correction of depreciation 
---
+++ src/Controller/UserController.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Form\UserType;
+// use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use App\Repository\UserRepository;
+// use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+// use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class UserController extends AbstractController
+{
+    /**
+     * @Route("/users", name="user_list")
+     */
+    // public function listAction()
+    public function listAction(UserRepository $userRepository)
+    {
+        // return $this->render('user/list.html.twig', ['users' => $this->getDoctrine()->getRepository('AppBundle:User')->findAll()]);
+        // return $this->render('user/list.html.twig', ['users' => $this->getDoctrine()->getRepository('User::class')->findAll()]);
+        // return $this->render('user/list.html.twig', ['users' => $this->getDoctrine()->getRepository(User::class)->findAll()]);
+        return $this->render('user/list.html.twig', ['users' => $userRepository->findAll()]);
+    }
+
+    /**
+     * @Route("/users/create", name="user_create")
+     */
+    // public function createAction(Request $request)
+    // public function createAction(Request $request, UserPasswordEncoderInterface $passwordEncoder)
+    public function createAction(Request $request, UserPasswordHasherInterface $passwordHasher)
+    {
+        $user = new User();
+        $form = $this->createForm(UserType::class, $user);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            // $password = $this->get('security.password_encoder')->encodePassword($user, $user->getPassword());
+            // $user->setPassword($password);
+            //  ----RAJOUT NICOLAS-------------
+            $Password = $form->get('password')->getData();
+            // $user->setPassword($passwordEncoder->encodePassword($user, $Password));
+            $user->setPassword($passwordHasher->hashPassword($user, $Password));
+            //  ----FIN RAJOUT NICOLAS-------------
+
+            $em->persist($user);
+            $em->flush();
+
+            $this->addFlash('success', "L'utilisateur a bien été ajouté.");
+
+            return $this->redirectToRoute('user_list');
+        }
+
+        return $this->render('user/create.html.twig', ['form' => $form->createView()]);
+    }
+
+    /**
+     * @Route("/users/{id}/edit", name="user_edit")
+     */
+    // public function editAction(User $user, Request $request)
+    // public function editAction(User $user, Request $request, UserPasswordEncoderInterface $passwordEncoder)
+    public function editAction(User $user, Request $request, UserPasswordHasherInterface $passwordHasher)
+
+    {
+        $form = $this->createForm(UserType::class, $user);
+
+        $form->handleRequest($request);
+
+        // if ($form->isValid()) {
+        if ($form->isSubmitted() && $form->isValid()) {
+            // $password = $this->get('security.password_encoder')->encodePassword($user, $user->getPassword());
+            // $user->setPassword($password);
+            $Password = $form->get('password')->getData();
+            // $user->setPassword($passwordEncoder->encodePassword($user, $Password));
+            $user->setPassword($passwordHasher->hashPassword($user, $Password));
+
+            $this->getDoctrine()->getManager()->flush();
+
+            $this->addFlash('success', "L'utilisateur a bien été modifié");
+
+            return $this->redirectToRoute('user_list');
+        }
+
+        return $this->render('user/edit.html.twig', ['form' => $form->createView(), 'user' => $user]);
+    }
+}
