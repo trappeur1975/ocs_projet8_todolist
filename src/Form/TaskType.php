b35f15efe0c42181662ac2ff95623b20aebaf312
Tr@ppeur1975 <trappeur1975photmail.com>
2022-01-30 12:04:13 +0400

    update of TaskType.php and edit.html.twig file

--- src/Form/TaskType.php
+++ src/Form/TaskType.php
@@ -2,8 +2,10 @@
 
 namespace App\Form;
 
+use App\Entity\Task;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
 class TaskType extends AbstractType
@@ -15,5 +17,13 @@ class TaskType extends AbstractType
             ->add('content', TextareaType::class)
             //->add('author') ===> must be the user authenticated
         ;
+    }
+
+    // rajout nico
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Task::class,
+        ]);
     }
 }
